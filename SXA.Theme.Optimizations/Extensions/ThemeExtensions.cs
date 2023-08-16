c1458c9f5a4868c3e2b90dffaf764fa2b4e9e575
Austin Gilbert <agilbert@rbaconsulting.com>
2023-08-16 13:20:05 -0500

    More hotfixes

--- SXA.Theme.Optimizations/Extensions/ThemeExtensions.cs
+++ SXA.Theme.Optimizations/Extensions/ThemeExtensions.cs
@@ -16,22 +16,38 @@ namespace SXA.Theme.Optimizations.Extensions
         /// <returns>A string for a html script tag's src attribute.</returns>
         public static string GetSXAThemeOptimizationsScript()
         {
-            var themingContext = ServiceLocator.ServiceProvider.GetService<IThemingContext>();
-            var scriptUrl = string.Format(FileNames.NewlyOptimizedMin, themingContext?.ThemeItem?.Name.Replace(" ", "-").ToLower(), themingContext?.ThemeItem?.Database?.Name.ToLower());
+            var scriptUrl = GetWebModulesPath();
+
+            if (Settings.GetBoolSetting(SitecoreSettings.AlwaysAppendRevision, false))
+            {
+                var updatedDate = File.GetLastWriteTimeUtc(GetFullFileSystemPath());
+
+                scriptUrl = $"{scriptUrl}?rev={updatedDate:MMddHHmmss}";
+            }
 
             if (Settings.GetBoolSetting(SitecoreSettings.AlwaysIncludeServerUrl, false))
             {
                 scriptUrl = Settings.GetSetting(SitecoreSettings.MediaLinkServerUrl, string.Empty) + scriptUrl;
             }
 
-            if (Settings.GetBoolSetting(SitecoreSettings.AlwaysAppendRevision, false))
-            {
-                var updatedDate = File.GetLastWriteTimeUtc($"{HttpRuntime.AppDomainAppPath}{scriptUrl.TrimStart('/').Replace("/", "\\")}");
-
-                scriptUrl = $"{scriptUrl}?rev={updatedDate:MMddHHmmss}";
-            }
-
             return scriptUrl;
+        }
+
+        public static string GetWebModulesPath()
+        {
+            var themingContext = ServiceLocator.ServiceProvider.GetService<IThemingContext>();
+
+            var themeName = themingContext?.ThemeItem?.Name?.Replace(" ", "-").ToLower() ?? string.Empty;
+            var databaseName = themingContext?.ThemeItem?.Database?.Name?.ToLower() ?? string.Empty;
+
+            return string.Format(FileNames.NewlyOptimizedMin, themeName, databaseName) ?? string.Empty;
+        }
+
+        public static string GetFullFileSystemPath()
+        {
+            var scriptPath = GetWebModulesPath()?.TrimStart('/').Replace("/", "\\") ?? string.Empty;
+
+            return $"{HttpRuntime.AppDomainAppPath}{scriptPath}" ?? string.Empty;
         }
     }
 }
