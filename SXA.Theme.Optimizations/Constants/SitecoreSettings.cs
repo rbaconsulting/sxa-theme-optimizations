f962af031e7e82852372950d1ab0251ac77bb8b9
Austin Gilbert <agilbert@rbaconsulting.com>
2023-08-16 11:36:49 -0500

    Removing support for security headers and also fixing bug for CDN caching

--- SXA.Theme.Optimizations/Constants/SitecoreSettings.cs
+++ SXA.Theme.Optimizations/Constants/SitecoreSettings.cs
@@ -2,9 +2,9 @@
 {
 	public struct SitecoreSettings
 	{
-		public const string AddSecurityHeaders = "SXA.Theme.Optimizations.AddSecurityHeaders";
 		public const string GenerateScriptsOnStartup = "SXA.Theme.Optimizations.GenerateScriptsOnStartup";
         public const string AlwaysIncludeServerUrl = "Media.AlwaysIncludeServerUrl";
         public const string MediaLinkServerUrl = "Media.MediaLinkServerUrl";
+        public const string AlwaysAppendRevision = "Media.AlwaysAppendRevision";
     }
 }
