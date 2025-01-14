162f52c5fd5ab7ce0df7c22d57d852fa5ef24b8b
Austin Gilbert <agilbert@rbaconsulting.com>
2025-01-14 17:41:54 -0600

    Fixing bug where wrong item name is used to generate script

--- SXA.Theme.Optimizations/Models/ThemeOptimizationSettings.cs
+++ SXA.Theme.Optimizations/Models/ThemeOptimizationSettings.cs
@@ -6,7 +6,6 @@ using SXA.Theme.Optimizations.Enums;
 using System;
 using System.IO;
 using System.Linq;
-using System.Web;
 using Templates = SXA.Theme.Optimizations.Constants.Templates;
 
 namespace SXA.Theme.Optimizations.Models
@@ -18,46 +17,31 @@ namespace SXA.Theme.Optimizations.Models
         public bool DeferCss { get; set; } = true;
         public string ScriptUrl { get; set; } = string.Empty;
 
-        public ThemeOptimizationSettings(Item item)
+        public ThemeOptimizationSettings(Item themeItem)
         {
-            if (item?.TemplateID == Templates.Theme.ID)
+            var themeOptimizationSettingsItem = themeItem.Children.FirstOrDefault(c => c.TemplateID == Templates.ThemeOptimizationSettings.ID);
+            if (themeOptimizationSettingsItem?.TemplateID == Templates.ThemeOptimizationSettings.ID)
             {
-                ScriptUrl = GetSXAThemeOptimizationsScript(item);
-                item = item.Children.FirstOrDefault(c => c.TemplateID == Templates.ThemeOptimizationSettings.ID);
-            }
-
-            if (item?.TemplateID == Templates.ThemeOptimizationSettings.ID)
-            {
-                var parseResult = Enum.TryParse(item[Templates.ThemeOptimizationSettings.Fields.RenderScriptsAs], out ScriptAttributes _renderScriptsAs);
+                var parseResult = Enum.TryParse(themeOptimizationSettingsItem[Templates.ThemeOptimizationSettings.Fields.RenderScriptsAs], out ScriptAttributes _renderScriptsAs);
                 RenderScriptsAs = parseResult ? _renderScriptsAs : ScriptAttributes.Async;
 
-                parseResult = Enum.TryParse(item[Templates.ThemeOptimizationSettings.Fields.ScriptsLocation], out ScriptLocations _scriptsLocation);
+                parseResult = Enum.TryParse(themeOptimizationSettingsItem[Templates.ThemeOptimizationSettings.Fields.ScriptsLocation], out ScriptLocations _scriptsLocation);
                 ScriptsLocation = parseResult ? _scriptsLocation : ScriptLocations.Top;
 
-                CheckboxField deferCssField = item.Fields[Templates.ThemeOptimizationSettings.Fields.DeferCSS];
+                CheckboxField deferCssField = themeOptimizationSettingsItem.Fields[Templates.ThemeOptimizationSettings.Fields.DeferCSS];
                 DeferCss = deferCssField?.Checked ?? true;
-
-                if (string.IsNullOrWhiteSpace(ScriptUrl) && item.Parent.TemplateID == Templates.Theme.ID)
-                {
-                    ScriptUrl = GetSXAThemeOptimizationsScript(item);
-                }
             }
-        }
 
-        private string GetSXAThemeOptimizationsScript(Item item)
-        {
-            if (!string.IsNullOrWhiteSpace(item.Parent?.Name) && !string.IsNullOrWhiteSpace(item.Parent?.Database?.Name))
+            if (!string.IsNullOrWhiteSpace(themeItem?.Name) && !string.IsNullOrWhiteSpace(themeItem?.Database?.Name))
             {
                 var alwaysIncludeServerUrl = Settings.GetBoolSetting(SitecoreSettings.AlwaysIncludeServerUrl, false);
                 var scriptUrlBase = alwaysIncludeServerUrl ? Settings.GetSetting(SitecoreSettings.MediaLinkServerUrl, string.Empty) : string.Empty;
 
-                var scriptUrl = $"{scriptUrlBase}{string.Format(FileNames.NewlyOptimizedMin, item.Parent.Name.Replace(" ", "-").ToLower(), item.Parent.Database.Name.ToLower())}";
+                var scriptUrl = $"{scriptUrlBase}{string.Format(FileNames.NewlyOptimizedMin, themeItem.Name.Replace(" ", "-").ToLower(), themeItem.Database.Name.ToLower())}";
 
                 var alwaysAppednRevision = Settings.GetBoolSetting(SitecoreSettings.AlwaysAppendRevision, false);
-                return alwaysAppednRevision ? $"{scriptUrl}?rev={File.GetLastWriteTimeUtc(scriptUrl):MMddHHmmss}" : scriptUrl;
+                ScriptUrl = alwaysAppednRevision ? $"{scriptUrl}?rev={File.GetLastWriteTimeUtc(scriptUrl):MMddHHmmss}" : scriptUrl;
             }
-
-            return string.Empty;
         }
     }
 }
